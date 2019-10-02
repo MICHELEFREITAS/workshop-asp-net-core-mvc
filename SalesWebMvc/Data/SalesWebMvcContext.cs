ddd9715f6cc4a35e75e6b0f47913c20ba197a5be
MICHELEFREITAS <michele-braga@hotmail.com>
2019-10-02 22:06:17 +0100

    MySQL adaptation and first migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Models
 {
+    //class básica Entity Framework que herda DBContext. Encapsula sessão com BD
     public class SalesWebMvcContext : DbContext
     {
         public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
