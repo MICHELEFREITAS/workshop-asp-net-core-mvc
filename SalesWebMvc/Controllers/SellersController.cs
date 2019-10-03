7dae5a8b902928bdfd80087a12a5fd4d76d90996
MICHELEFREITAS <michele-braga@hotmail.com>
2019-10-03 16:09:52 +0100

    SellersController

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    //recebe a chamada do caminho /Sellers quando clica no link da pág
+    public class SellersController : Controller
+    {
+        //aciona view que está na pasta Sellers com nome index. O controlador encaminhou requisição para view. Assim é o MVC
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
